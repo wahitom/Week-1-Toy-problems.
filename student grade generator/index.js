ecfd0e65be5c38ce7e21441d8960cdb173eda8ab
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-25 11:26:03 +0300

    Added a prompt

--- student grade generator/index.js
+++ student grade generator/index.js
@@ -1,23 +1,29 @@
-function inputMarks(num1){
-    num1; 
+const button = document.getElementById("btn");
 
-    if(num1 > 79 && num1 <= 100){
-        console.log("You have an A");
+button.addEventListener('click', function(){
+
+    const userInput = prompt("Please enter your Marks");
+
+    if(userInput === null || userInput === NaN){
+        alert("Invalid input")
     }
-    else if(num1 >= 60 && num1 <=79){
-        console.log("You have a B");
+    else if(userInput > 79 && userInput <= 100){
+        alert("You have an A");
     }
-    else if(num1 < 49 && num1 >= 40){
-        console.log("You have a C");
+    else if(userInput >= 60 && userInput <=79){
+        alert("You have a B");
     }
-    else if(num1 < 40 && num1 >= 0){
-        console.log("You have an E");
+    else if(userInput >= 49 && userInput <= 59){
+        alert("You have a C");
+    }
+    else if(userInput >= 40 && userInput <= 49){
+        alert("You have a D")
+    }
+    else if(userInput < 40 && userInput >= 0){
+        alert("You have an E");
     }
     else{
-        console.log("invalid input")
+        alert("invalid input")
     }
 
-    return num1;
-}
-
-console.log(inputMarks(101));
+})
