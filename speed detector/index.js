4fc813ca53709bc1e9e8af584660b37cc5e98428
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-25 11:36:07 +0300

    Added a prompt for speed detector

--- speed detector/index.js
+++ speed detector/index.js
@@ -1,31 +1,25 @@
 
+const button = document.getElementById("btn");
 
-//function that calculates the speed and demerits
-
-
-function calculateSpeed(num1){  //input our speed here in num1
-    //const prompt1 = prompt("Enter the car's speed (in km/h):")
+button.addEventListener('click', function(){
+    const userInput = prompt("Please enter your Speed");
 
     const speedLimit = 70;      
-    const overSpeed = num1 - speedLimit //difference between speed and speedLimit
+    const overSpeed = userInput - speedLimit //difference between speed and speedLimit
     let demerit = Math.round(overSpeed/5) //calculate the demerit by dividing the overspeed by 5
 
-    if(num1 < speedLimit){ //if number is below speed limit
-        console.log("Ok")
+    if(userInput < speedLimit){ //if number is below speed limit
+        alert("Ok")
     }
-    else if(num1 > speedLimit && num1 <= 130){ //if number is above speed limit but still below 130
+    else if(userInput > speedLimit && userInput <= 130){ //if number is above speed limit but still below 130
         //demerit = (overSpeed/5);
-        console.log(`You have ${demerit} demerit points`)
+        alert(`You have ${demerit} demerit points`)
     }
-    else if(num1 > 130){ //if number is above 130 
-        console.log("License Suspended ")
+    else if(userInput > 130){ //if number is above 130 
+        alert("License Suspended ")
     }
 
-    return num1;
-}
-
-console.log(calculateSpeed(131))
-
+});
 
 
 
